8126cf9e770b34827cb8dcd5a2b15a23297421ad
Fabio N. Filasieno <59163015+filasieno@users.noreply.github.com>
2024-10-26 14:23:27 +0200

    Add a single type and instance to collect all global variables

---
+++ include/srv0state.h
@@ -0,0 +1,46 @@
+/****************************************************************************
+Copyright (c) 2024 Sunny Bains. All rights reserved.
+
+Portions of this file contain modifications contributed and copyrighted by
+Google, Inc. Those modifications are gratefully acknowledged and are described
+briefly in the InnoDB documentation. The contributions by Google are
+incorporated with their permission, and subject to the conditions contained in
+the file COPYING.Google.
+
+Portions of this file contain modifications contributed and copyrighted
+by Percona Inc.. Those modifications are
+gratefully acknowledged and are described briefly in the InnoDB
+documentation. The contributions by Percona Inc. are incorporated with
+their permission, and subject to the conditions contained in the file
+COPYING.Percona.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+
+*****************************************************************************/
+
+#ifndef SRV0STATE_H
+#define SRV0STATE_H
+
+
+// Define a single InnoDB global state type.
+// The type holds defines the state of a single instace of the InnoDB.
+// All fields will be prefixed to highlight the module the defines it.
+struct InnoDB_state {
+
+};
+
+extern InnoDB_state state;
+
+
+
+#endif //SRV0STATE_H
